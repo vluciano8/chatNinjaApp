eace8f0ff4db13dc54ddd1bd8fbc0110c9d38397
vluciano8 <vluciano88@gmail.com>
2019-09-08 20:24:37 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// dom queries
+const chatlist = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
+//add new chat
+newChatForm.addEventListener('submit', e =>{
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // update name via chatroom class
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMssg.innerText = `Tu alias fue actualizado a: ${newName}`;
+  setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+// update the chatroom
+rooms.addEventListener('click', e => {
+  if (e.target.tagName === 'BUTTON'){
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+})
+
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon';  
+
+// class instances
+const chatUI = new ChatUI(chatlist);
+const chatroom = new Chatroom ('general', username);
+
+// get chats and render
+chatroom.getChats(data => chatUI.render(data));
+
+// reset all messages 
